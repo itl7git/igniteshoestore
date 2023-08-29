cd8d6fb88bb1cc5b27650a9992b61c06f4e834bb
Italo Leal <itlcoorporativo@gmail.com>
2023-08-29 14:44:05 -0300

    feat: Estruturação do projeto com o OneSignal e Expo Dev Client

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import { StatusBar } from 'react-native';
+import { OneSignal } from 'react-native-onesignal';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
@@ -8,6 +9,10 @@ import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+
+const ONE_SIGNAL = '457624c0-32c7-49b2-9238-f11cedf879f2'
+OneSignal.initialize(ONE_SIGNAL)
+
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
